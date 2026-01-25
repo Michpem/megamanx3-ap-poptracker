1c4cc53da303e7831957ccf1a20598b75ac1639b
Michpem <michaelssbb@gmail.com>
2026-01-25 12:25:32 -0600

    1.0.5 update

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -1,6 +1,7 @@
 ITEM_MAPPING = {
     [12386304] = {"victory", "toggle"},
     [12386305] = {"maverick_medal", "consumable"},
+	
     [12386306] = {"stage_toxic_seahorse", "toggle"},
     [12386307] = {"stage_volt_catfish", "toggle"},
     [12386308] = {"stage_tunnel_rhino", "toggle"},
@@ -11,6 +12,7 @@ ITEM_MAPPING = {
     [12386313] = {"stage_blast_hornet", "toggle"},
     [12386314] = {"stage_doppler_lab", "toggle"},
     [12386329] = {"stage_vile", "toggle"},
+	
     [12386315] = {"frost_shield", "toggle"},
     [12386316] = {"acid_burst", "toggle"},
     [12386317] = {"tornado_fang", "toggle"},
@@ -19,20 +21,34 @@ ITEM_MAPPING = {
     [12386320] = {"ray_splasher", "toggle"},
     [12386321] = {"gravity_well", "toggle"},
     [12386322] = {"parasitic_bomb", "toggle"},
+	
     [12386330] = {"z_saber", "toggle"},
+	
     [12386323] = {"heart_tank", "consumable"},
     [12386324] = {"sub_tank", "consumable"},
+	
     [12386325] = {"ride_chimera", "toggle"},
     [12386326] = {"ride_kangaroo", "toggle"},
     [12386327] = {"ride_hawk", "toggle"},
     [12386328] = {"ride_frog", "toggle"},
+	
     [12386332] = {"third_armor_helmet", "progressive"},
     [12386333] = {"third_armor_body", "progressive"},
     [12386334] = {"third_armor_arms", "progressive"},
     [12386335] = {"third_armor_legs", "progressive"},
+	
     [12386352] = {"small_hp", "consumable"},
     [12386353] = {"large_hp", "consumable"},
     [12386354] = {"small_weapon", "consumable"},
     [12386355] = {"large_weapon", "consumable"},
     [12386356] = {"life", "consumable"},
+	
+	[12386368] = {"chip_quick_charge", "toggle"},
+	[12386369] = {"chip_speedster", "toggle"},
+	[12386370] = {"chip_super_recover", "toggle"},
+	[12386371] = {"chip_rapid_five", "toggle"},
+	[12386372] = {"chip_speed_shot", "toggle"},
+	[12386373] = {"chip_weapon_plus", "toggle"},
+	[12386374] = {"chip_buster_plus", "toggle"},
+	[12386375] = {"chip_item_plus", "toggle"}
 }
